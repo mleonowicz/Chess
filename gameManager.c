c9e18280691c6197eaf0d3ecb0d6285999dd8ccb
wonziu <wonziu@pop-os.localdomain>
2020-02-04 19:56:05 +0100

    Forgot to add the file

--- gameManager.c
+++ gameManager.c
@@ -93,6 +93,31 @@ bool explode(int array[8][8], Position tile) {
     return lost;
 }
 
+bool legalMove(Position from, Position to, int piece, bool attacking, int turn, int array[8][8]) {
+    switch(piece) {
+        case 1:
+            return legalMovePawn(from, to, attacking, turn, array); 
+        break;
+        case 2:
+            return legalMoveBishop(array, from, to);
+        break;
+        case 3:
+            return legalMoveKnight(from, to);
+        break;
+        case 4:
+            return legalMoveRook(array, from, to);
+        break;
+        case 5:
+            return (legalMoveBishop(array, from, to) || legalMoveRook(array, from, to));
+        break;
+        case 6:
+            return legalMoveKing(from, to, attacking);
+        break;
+    }
+
+    return false;
+}
+
 bool isTileAttacked(int array[8][8], Position tile, int t) { // t for enemy piece
     for (int x = 0; x < 8; x++) {
         for (int y = 0; y < 8; y++) {
@@ -101,37 +126,50 @@ bool isTileAttacked(int array[8][8], Position tile, int t) { // t for enemy piec
                 pos.x = x;
                 pos.y = y;
 
-                switch (array[x][y] * t) {
-                    case 1:
-                        if (legalMovePawn(pos, tile, true, t))
-                            return true;
-                    break;
-                    case 2:
-                        if (legalMoveBishop(array, pos, tile))
-                            return true;
-                    break;
-                    case 3:
-                        if (legalMoveKnight(pos, tile))
-                            return true;
-                    break;
-                    case 4:
-                        if (legalMoveRook(array, pos, tile))
-                            return true;
-                    break;
-                    case 5:
-                        if (legalMoveBishop(array, pos, tile) || legalMoveRook(array, pos, tile))
-                            return true;
-                    break;
-                }
+                if (legalMove(pos, tile, array[x][y] * t, true, t, array))
+                    return true;
             }  
         }
     }
     return false;  
 }
 
+bool moveWouldPutInCheck(int array[8][8], Position from, Position to, int piece) {
+    bool illegalMove = false;
+    int arrayCopy[8][8];
+
+    memcpy(arrayCopy, array, sizeof(pieces));
+
+    arrayCopy[to.x][to.y] = arrayCopy[from.x][from.y];
+    arrayCopy[from.x][from.y] = 0;
+        
+    if (absolute(piece) == 6)
+        changeKingPos(turn, to);   
+
+    if (turn == 1) {
+        if (isTileAttacked(arrayCopy, whiteKing, -1))
+            illegalMove = true;
+    }
+    else {
+        if (isTileAttacked(arrayCopy, blackKing, 1))
+            illegalMove = true;
+    }
+
+    if (absolute(piece) == 6)
+        changeKingPos(turn, selectedPiecePos);
+
+    return illegalMove;
+}
+
 void castle(Position k, Position r) {
     Position newKing;
     Position newRook;
+
+    if (r.x == 4) {
+        Position temp = r;
+        r = k;
+        k = temp;
+    } 
 
     newKing.y = k.y;
     newRook.y = r.y;
@@ -143,40 +181,40 @@ void castle(Position k, Position r) {
     if (newRook.x == newKing.x)
         newKing.x += x;
 
-    placePiece(r, newRook);
+    movedRook(r);
+
     placePiece(k, newKing);
+    placePiece(r, newRook);
+
+    kingMoved(turn);
     changeKingPos(turn, newKing);
-    
-    if (!isTileAttacked(pieces, turn == 1 ? whiteKing : blackKing, turn * -1)) { // if castle would put king in a check
-        placePiece(k, newKing);
-        changeKingPos(turn, newKing);
-        kingMoved(turn); // changing kings first move
 
-        placePiece(r, newRook);
-        movedRook(newRook);
-
-        turn *= -1;
-    }
-    else {
-        placePiece(newRook, r);
-        placePiece(newKing, k);
-        changeKingPos(turn, k);
-    }
+    turn *= -1;
 }
 
-bool checkCastle(int newPiece, Position pos) {
+bool canCastle(int pieceFrom, int pieceTo, Position from, Position to, int t) {
     Position kingPos;
     Position rookPos;
 
-    if (absolute(selectedPiece) == 4 && absolute(newPiece) == 6) {
-        kingPos = pos;
-        rookPos = selectedPiecePos;
+    if (absolute(pieceFrom) == 4 && absolute(pieceTo) == 6) {
+        kingPos = to;
+        rookPos = from;
     }
-    else if (absolute(newPiece) == 4 && absolute(selectedPiece) == 6) {
-        kingPos = selectedPiecePos;
-        rookPos = pos;
+    else if (absolute(pieceTo) == 4 && absolute(pieceFrom) == 6) {
+        kingPos = from;
+        rookPos = to;
     }
     else 
+        return false;
+
+    if (checkIfRookMoved(rookPos))
+        return false;
+
+    if (t == 1) {
+        if (whiteKingMoved)
+            return false;
+    }
+    else if (blackKingMoved)
         return false;
 
     int x = (kingPos.x - rookPos.x) > 0 ? -1 : 1;
@@ -191,71 +229,88 @@ bool checkCastle(int newPiece, Position pos) {
         newX += x;
     }
 
-    if (!checkIfRookMoved(rookPos)) {
-        if (turn == 1) {
-            if (!whiteKingMoved) {
-                castle(kingPos, rookPos);
-                return true;
-            }
-        }
-        else {
-            if (!blackKingMoved) {
-                castle(kingPos, rookPos);
-                return true;
-            }
-        }
-    }
+    Position newKing;
+    Position newRook;
 
-    return false;
+    newKing.y = kingPos.y;
+    newRook.y = rookPos.y;
+
+    x = (kingPos.x - rookPos.x) > 0 ? -1 : 1;
+    newKing.x = kingPos.x + x + x;
+    newRook.x = rookPos.x - x - x;
+
+    if (newRook.x == newKing.x)
+        newKing.x += x;
+
+    movePiece(kingPos, newKing);
+    movePiece(rookPos, newRook);
+    changeKingPos(t, newKing);
+
+    bool canCastle = true;
+
+    // if castle would put king in a check
+    if (isTileAttacked(pieces, t == 1 ? whiteKing : blackKing, t * -1))
+        canCastle = false;
+
+    movePiece(newKing, kingPos);
+    movePiece(newRook, rookPos);
+    changeKingPos(t, kingPos);
+
+    return canCastle;
+}
+
+void removeStyle() {
+    for (int x = 0; x < 8; x++)
+        for (int y = 0; y < 8; y++) 
+            setStyleSelected(x, y, false);
+}
+
+void drawMoves(Position pos) {
+    int piece = pieces[pos.x][pos.y];
+
+    for (int x = 0; x < 8; x++)
+            for (int y = 0; y < 8; y++) {
+                Position newPos;
+                newPos.x = x;
+                newPos.y = y;
+                int pieceTemp = pieces[x][y];
+
+                if (!sameSign(pieceTemp, turn)) {
+                    if (legalMove(pos, newPos, absolute(piece), attacking(newPos), turn, pieces)) 
+                        if (!moveWouldPutInCheck(pieces, pos, newPos, absolute(piece)))
+                            setStyleSelected(x, y, true);  
+                }
+                else if (isTileAttacked(pieces, turn == 1 ? blackKing : whiteKing, turn) && canCastle(piece, pieceTemp, pos, newPos, turn))
+                    setStyleSelected(x, y, true);  
+            }
 }
 
 void handleClick(Position pos) {
     int piece = pieces[pos.x][pos.y];
+
     if (sameSign(piece, turn)) {
+        removeStyle();
 
-        if(checkCastle(piece, pos))
+        if (isTileAttacked(pieces, turn == 1 ? blackKing : whiteKing, turn) && canCastle(selectedPiece, piece, selectedPiecePos, pos, turn)) {
+            castle(selectedPiecePos, pos);
+            printBoard(pieces);
             return;
+        }
 
-        setStyleSelected(selectedPiecePos.x, selectedPiecePos.y, false);
         setStyleSelected(pos.x, pos.y, true);
         
         selectedPiecePos = pos;
         selectedPiece = piece;
-    }
-    else if (selectedPiece != 0) 
-    {
-        bool correctMove = false;
 
-        switch(absolute(selectedPiece)) {
-            case 1:
-                if (legalMovePawn(selectedPiecePos, pos, attacking(pos), turn))
-                    correctMove = true;
-            break;
-            case 2:
-                if (legalMoveBishop(pieces, selectedPiecePos, pos))
-                    correctMove = true;               
-            break;
-            case 3:
-                if (legalMoveKnight(selectedPiecePos, pos))
-                    correctMove = true;
-            break;
-            case 4:
-                if (legalMoveRook(pieces, selectedPiecePos, pos)) {
-                    // movedRook(selectedPiecePos);
-                    correctMove = true;
-                }
-            break;
-            case 5: 
-                if (legalMoveRook(pieces, selectedPiecePos, pos) || legalMoveBishop(pieces, selectedPiecePos, pos)) // queen movement is same as rook and bishop
-                    correctMove = true;
-            break;
-            case 6:
-                if (legalMoveKing(selectedPiecePos, pos, attacking(pos))) {
-                    // movedKing(turn, pos);
-                    correctMove = true;
-                }
-            break;
-        }
+        drawMoves(pos);
+    }
+    else if (selectedPiece != 0) {
+        bool correctMove = legalMove(selectedPiecePos, pos, absolute(selectedPiece), attacking(pos), 1, pieces);
+        bool pawnMove = false;
+
+        // printBoard(pieces);
+        // printf("WHITE: X:%d Y:%d\n", whiteKing.x, whiteKing.y);
+        // printf("BLACK: X:%d Y:%d\n", blackKing.x, blackKing.y);
 
         if (!correctMove)
             return;
@@ -263,50 +318,38 @@ void handleClick(Position pos) {
         int array[8][8];
         memcpy(array, pieces, sizeof(pieces));
         
-        if (attacking(pos)) {
+        // checking if can kill king in one turn under check without killing own king
+        if (attacking(pos) || (pawnMove && (pos.y == 0 || pos.y == 7))) {
             explode(array, pos);
             array[selectedPiecePos.x][selectedPiecePos.y] = 0;
         }
 
-        if (isKingDead(array, turn * -1) && !isKingDead(array, turn)) { // checking if can kill king in one turn under check
-                placePiece(selectedPiecePos, pos);
-                explode(pieces, pos);
+        if (isKingDead(array, turn * -1) && !isKingDead(array, turn)) { 
+            placePiece(selectedPiecePos, pos);
+            explode(pieces, pos);
                 
-                printf("lost\n");
-                fflush(stdout);
-                return;
-        }
-
-        // checkin if move would put king in a check
-        bool illegalMove = false;
-
-        array[pos.x][pos.y] = array[selectedPiecePos.x][selectedPiecePos.y];
-
-        if (absolute(selectedPiece) == 6)
-            changeKingPos(turn, pos);   
-
-        if (turn == 1) {
-            if (isTileAttacked(array, whiteKing, -1))
-                illegalMove = true;
-        }
-        else {
-            if (isTileAttacked(array, blackKing, 1))
-                illegalMove = true;
-        }
-
-        if (illegalMove) {
-            if (absolute(selectedPiece) == 6)
-                changeKingPos(turn, selectedPiecePos);
-
+            printf("lost\n");
+            fflush(stdout);
+            return;
+        } else if (isKingDead(array, turn)) {
+            printf("you cant kill your own king\n");
+            fflush(stdout);
             return;
         }
 
-        if (absolute(selectedPiece) == 6) // making move
+        // checkin if move would put king in a check
+        if (moveWouldPutInCheck(pieces, selectedPiecePos, pos, absolute(selectedPiece)))
+            return;
+
+        // making move
+        if (absolute(selectedPiece) == 6) { 
+            changeKingPos(turn, selectedPiecePos);
             kingMoved(turn);
+        }
         else if (absolute(selectedPiece) == 4)
             movedRook(selectedPiecePos);
 
-        if (attacking(pos)) {
+        if (attacking(pos) || (pawnMove && (pos.y == 0 || pos.y == 7))) {
             placePiece(selectedPiecePos, pos);
             if (explode(pieces, pos))   {
                 printf("lost\n");
@@ -317,6 +360,7 @@ void handleClick(Position pos) {
             placePiece(selectedPiecePos, pos);
 
         turn *= -1;
+        removeStyle();
     }
 }
 
